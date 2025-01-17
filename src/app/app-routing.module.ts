292fbb96d664a5ebc1f88e81b8ef27d1ca7c5ba1
QuangVinh <vinhdqde170663@fpt.edu.vn>
2025-01-17 18:01:40 +0700

    push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageRoomComponent } from './Components/home-page-room/home-page-room.component';
 
-const routes: Routes = [{ path: 'RoomPage', component: HomePageRoomComponent }];
+const routes: Routes = [{ path: '', component: HomePageRoomComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
