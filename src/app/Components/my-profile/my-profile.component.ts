63bdf6ca0c2cc8c5ff26828d49e28c0fecca45e7
mdao2211 <superhero21442922@gmail.com>
2025-02-09 20:46:12 +0700

    my-profile-angular

---
+++ src/app/Components/my-profile/my-profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-profile.component.html',
+  styleUrl: './my-profile.component.css',
+})
+export class MyProfileComponent {}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageRoomComponent } from './Components/home-page-room/home-page-room.component';
+import { MyProfileComponent } from './Components/my-profile/my-profile.component';
 
-const routes: Routes = [{ path: '', component: HomePageRoomComponent }];
+const routes: Routes = [
+  { path: '', component: HomePageRoomComponent },
+  { path: 'my-profile', component: MyProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
