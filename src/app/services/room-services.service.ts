22b93b3ca858abdd94e86377eff02b2ab86523c2
twna21 <tuongnmde170578@fpt.edu.vn>
2025-02-21 22:49:45 +0700

    fix domain url

--- src/app/services/room-services.service.ts
+++ src/app/services/room-services.service.ts
@@ -5,14 +5,18 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class RoomServicesService {
-  url = 'http://dev-vmeet.runasp.net/rooms';
+  private readonly BASE_URL = 'http://dev-vmeet.runasp.net/rooms';
 
   constructor(private http: HttpClient) {}
 
   getRooms(top: number, skip: number): any {
-    return this.http.get<any>(this.url + '?Top=' + top + '&Skip=' + skip);
+    const url = `${this.BASE_URL}?NeedTotalCount=true&Top=${top}&Skip=${skip}`;
+    return this.http.get<any>(url);
   }
+
   deleteRoom(id: string): any {
-    return this.http.delete<any>(this.url + '/' + id);
+    const deleteUrl = `${this.BASE_URL}/${id}`;
+    return this.http.delete<any>(deleteUrl);
   }
 }
+
