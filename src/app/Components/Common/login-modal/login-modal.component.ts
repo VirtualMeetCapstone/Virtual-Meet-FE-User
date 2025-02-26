8db6439e7478d235f58a89e2cd6fe839993d2bb1
twna21 <tuongnmde170578@fpt.edu.vn>
2025-02-26 13:06:06 +0700

    fix modal popUP-login

--- src/app/Components/Common/login-modal/login-modal.component.ts
+++ src/app/Components/Common/login-modal/login-modal.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit, inject,EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output, inject } from '@angular/core';
 import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { HttpClient } from '@angular/common/http';
 import { AppConstants } from '../../../constant/AppConstants';
+
 @Component({
   selector: 'app-login-modal',
   templateUrl: './login-modal.component.html',
   styleUrl: './login-modal.component.scss',
 })
-export class LoginModalComponent {
+export class LoginModalComponent implements OnInit {
   @Input() isShowLoginDialog: boolean = false;
-  @Output() openLoginDialog = new EventEmitter();
+  @Output() openLoginDialog = new EventEmitter<boolean>();
   user: SocialUser | null = null;
   loggedIn: boolean = false;
 
@@ -17,32 +18,36 @@ export class LoginModalComponent {
   private http = inject(HttpClient);
 
   onClickLoginDialog() {
-    this.isShowLoginDialog = !this.isShowLoginDialog;
-    this.openLoginDialog.emit(this.isShowLoginDialog);
+    this.openLoginDialog.emit(false); // ✅ Gửi event đóng modal lên Header
   }
 
   ngOnInit() {
     this.authService.authState.subscribe((user) => {
       this.user = user;
-      this.loggedIn = (user != null);
+      this.loggedIn = user != null;
       if (user) {
-       this.sendTokenToBackend(user.idToken);
-       console.log(user);
-       console.log(user.idToken);
+        console.log(user);
+       // this.sendTokenToBackend(user.idToken);
+       this.openLoginDialog.emit(false);
       }
     });
   }
 
   sendTokenToBackend(idToken: string) {
-    this.http.post('${AppConstants.API_BASE_URL}/signin/google', { token: idToken })
+    this.http.post(`${AppConstants.API_BASE_URL}/signin/google`, { token: idToken })
       .subscribe(response => {
         console.log('Server Response:', response);
+        this.openLoginDialog.emit(false); // ✅ Đóng modal sau khi đăng nhập thành công
       }, error => {
         console.error('Error sending token:', error);
       });
   }
 
   signOut(): void {
-    this.authService.signOut();
+    this.authService.signOut().then(() => {
+      this.user = null;
+      this.loggedIn = false;
+      this.openLoginDialog.emit(false); // ✅ Đóng modal khi đăng xuất
+    });
   }
 }
