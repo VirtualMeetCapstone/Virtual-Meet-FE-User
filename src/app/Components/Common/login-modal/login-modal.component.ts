21e6d1af47be906cba91968a30e57535de7e0a67
twna21 <tuongnmde170578@fpt.edu.vn>
2025-02-28 11:09:15 +0700

    update login google

--- src/app/Components/Common/login-modal/login-modal.component.ts
+++ src/app/Components/Common/login-modal/login-modal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, EventEmitter, Input, Output, inject } from '@angular
 import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { HttpClient } from '@angular/common/http';
 import { AppConstants } from '../../../constant/AppConstants';
+import { AuthService } from '../../../services/auth-service/auth.service'; // Import AuthService để cập nhật trạng thái
 
 @Component({
   selector: 'app-login-modal',
@@ -16,6 +17,7 @@ export class LoginModalComponent implements OnInit {
 
   private authService = inject(SocialAuthService);
   private http = inject(HttpClient);
+  private customAuthService: AuthService = inject(AuthService);
 
   onClickLoginDialog() {
     this.openLoginDialog.emit(false);
@@ -24,31 +26,45 @@ export class LoginModalComponent implements OnInit {
   ngOnInit() {
     this.authService.authState.subscribe((user) => {
       this.user = user;
-      this.loggedIn = user != null;
+      this.loggedIn = !!user;
+
       if (user) {
-        // console.log(user);
         this.sendTokenToBackend(user.idToken);
-       this.openLoginDialog.emit(false);
+        this.openLoginDialog.emit(false);
+        this.customAuthService.updateLoginState(true);
       }
     });
   }
 
   sendTokenToBackend(idToken: string) {
-    const url = `${AppConstants.API_BASE_URL}/signin/google?idToken=${encodeURIComponent(idToken)}`;
+    const url = `${AppConstants.API_LOCAL_BASE_URL}/signin/google?idToken=${encodeURIComponent(idToken)}`;
 
-    this.http.get(url).subscribe(response => {
-      // console.log('Server Response:', response);
-      this.openLoginDialog.emit(false);
-    }, error => {
-      console.error('Error sending token:', error);
-    });
+    this.http.get<{ accessToken: string; refreshToken: string }>(url).subscribe(
+      (response) => {
+        if (response?.accessToken) {
+          localStorage.setItem('accessToken', response.accessToken);
+          localStorage.setItem('refreshToken', response.refreshToken);
+
+          setTimeout(() => {
+            this.customAuthService.updateLoginState(true);
+          }, 100);
+        } else {
+          console.error('No accessToken in response:', response);
+        }
+      },
+      (error) => {
+        console.error('Error sending token:', error);
+      }
+    );
   }
+
 
   signOut(): void {
     this.authService.signOut().then(() => {
       this.user = null;
       this.loggedIn = false;
-      this.openLoginDialog.emit(false); 
+      this.customAuthService.updateLoginState(false);
+      this.openLoginDialog.emit(false);
     });
   }
 }
