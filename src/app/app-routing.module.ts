10c28415c2eb186e064f29fb3f8b901967ac826e
twna21 <tuongnmde170578@fpt.edu.vn>
2025-03-04 16:58:43 +0700

    feat: youtube tool - bug: cannot seekTo(time)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageRoomComponent } from './Components/home-page-room/home-page-room.component';
 import { MyProfileComponent } from './Components/my-profile/my-profile.component';
 import { HomePagePostComponent } from './Components/home-page-post/home-page-post.component';
+import { YoutubePlayerComponent } from './Components/youtube-player/youtube-player.component';
 const routes: Routes = [
   { path: '', component: HomePageRoomComponent },
   { path: 'my-profile/:id', component: MyProfileComponent },
   { path: 'posts', component: HomePagePostComponent },
+  { path: 'video', component: YoutubePlayerComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
