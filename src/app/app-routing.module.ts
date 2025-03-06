10e9278db56bd43089913c55158408f3740477d3
duchadev <duchadev145@gmail.com>
2025-03-07 00:51:25 +0700

    room template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { HomePageRoomComponent } from './Components/home-page-room/home-page-roo
 import { MyProfileComponent } from './Components/my-profile/my-profile.component';
 import { HomePagePostComponent } from './Components/home-page-post/home-page-post.component';
 import { YoutubePlayerComponent } from './Components/youtube-player/youtube-player.component';
+import {RoomComponentComponent} from "./Components/room-component/room-component.component";
 const routes: Routes = [
   { path: '', component: HomePageRoomComponent },
   { path: 'my-profile/:id', component: MyProfileComponent },
   { path: 'posts', component: HomePagePostComponent },
   { path: 'video', component: YoutubePlayerComponent },
+  { path: 'room', component: RoomComponentComponent },
+
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
