ec0dad0f204e3a2be96ad78ccbc58d334f570f83
duchadev <duchadev145@gmail.com>
2025-03-12 00:29:53 +0700

    create post feature

--- src/app/Components/home-page-post/home-page-post.component.ts
+++ src/app/Components/home-page-post/home-page-post.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { PostserviceService } from '../../services/post-service/postservice.service';
+import {Component, OnInit} from '@angular/core';
+import {PostserviceService} from '../../services/post-service/postservice.service';
+import {CreateStoryDialogComponent} from "../create-story-dialog/create-story-dialog.component";
+import {MatDialog, MatDialogRef} from "@angular/material/dialog";
+import {CreatePostModalComponent} from "../create-post-modal/create-post-modal.component";
 
 @Component({
   selector: 'app-home-page-post',
@@ -12,8 +15,10 @@ export class HomePagePostComponent implements OnInit {
   pageSize: number = 9;
   skip: number = 0;
   loading: boolean = false;
+  userId: string = '';
 
-  constructor(private postService: PostserviceService) {}
+  constructor(private postService: PostserviceService, private dialog: MatDialog) {
+  }
 
   ngOnInit(): void {
     this.loadMorePosts();
@@ -36,5 +41,33 @@ export class HomePagePostComponent implements OnInit {
         this.skip += this.pageSize;
         this.loading = false;
       });
+  }
+
+  createPost() {
+
+  }
+
+  createFeeling() {
+
+  }
+
+  tagFriend() {
+
+  }
+
+
+  openCreatePost() {
+    const dialogRef = this.dialog.open(CreatePostModalComponent, {
+      width: '500px',
+      data: {
+        id: this.userId,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        console.log('New story added:', result);
+      }
+    });
   }
 }
