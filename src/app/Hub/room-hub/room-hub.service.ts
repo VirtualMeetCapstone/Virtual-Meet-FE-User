4ede137c9811066d00276970949816db5db84a28
twna21 <tuongnmde170578@fpt.edu.vn>
2025-03-14 18:41:48 +0700

    fix url

--- src/app/Hub/room-hub/room-hub.service.ts
+++ src/app/Hub/room-hub/room-hub.service.ts
@@ -11,7 +11,7 @@ export class RoomHubService {
 
   constructor() {
     this.hubConnection = new signalR.HubConnectionBuilder()
-      .withUrl(`${AppConstants.API_BASE_URL_HTTP}/roomHub`, { withCredentials: true })
+      .withUrl(`${AppConstants.API_BASE_URL_HTTPS}/roomHub`, { withCredentials: true })
       .withAutomaticReconnect()
       .build();
   }
