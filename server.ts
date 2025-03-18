bc0bedbf25f976a092d61cd47c1958d493f5e7de
twna21 <tuongnmde170578@fpt.edu.vn>
2025-03-18 11:18:20 +0700

    fix: remove @angular/ssr

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import 'zone.js';
 import 'zone.js/node';
 import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr/node';
+import { CommonEngine } from '@angular/ssr';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
